1cdb4d9467940bb9036c055cbd55ffe0605e237f
HyunMinSam <hyunmin4287@naver.com>
2021-07-01 22:57:10 +0900

    dto Controller 생성 page 74

--- src/main/java/project/web/HelloController.java
+++ src/main/java/project/web/HelloController.java
@@ -1,7 +1,9 @@
 package project.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import project.web.dto.HelloResponseDto;
 
 @RestController // JSON 을 반환하는 컨트롤러로 만듬 -> ?
 public class HelloController {
@@ -9,5 +11,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("hello/dto")
+    public HelloResponseDto hellloDto (@RequestParam("name") String name, @RequestParam("amount") int amount) { // Requset Param으로 입력받아 객체 생성 후 리턴
+        return new HelloResponseDto(name, amount);
     }
 }
