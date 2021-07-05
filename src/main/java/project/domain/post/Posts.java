add2c9539a28cbcfeb3c81051491c5687ccd13a3
HyunMinSam <hyunmin4287@naver.com>
2021-07-05 23:39:16 +0900

    domain패키지에 Posts 객체 생성

---
+++ src/main/java/project/domain/post/Posts.java
@@ -0,0 +1,36 @@
+package project.domain.post;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor // 파라미터가 없는 기본 생성자를 생성해주고
+@Entity // JPA 어노테이션 , JPA에서는 엔티티는 테이블에 대응하는 하나의 클래스라고 생각
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
