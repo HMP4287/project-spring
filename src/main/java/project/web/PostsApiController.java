330ff1e93f9a5783d02f5dc6eab7544935ae901c
HyunMinSam <hyunmin4287@naver.com>
2021-07-06 23:05:34 +0900

    포스트 등록, 수정, 삭제 위한 클래스 생성

---
+++ src/main/java/project/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package project.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import project.service.PostsService;
+import project.web.dto.PostsSaveRequestDto;
+
+@RequiredArgsConstructor // final 선언된 필드 생성자 만들어줌 ->
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+}
