fffb09848551432772808ef61a5bc62b4ad398f8
HyunMinSam <hyunmin4287@naver.com>
2021-07-18 00:58:47 +0900

    게시글 조회 controller, service, repository, dto 생성 및 수정

--- src/main/java/project/service/PostsService.java
+++ src/main/java/project/service/PostsService.java
@@ -4,11 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import project.domain.posts.Posts;
 import project.domain.posts.PostsRepository;
+import project.web.dto.PostsListResponseDto;
 import project.web.dto.PostsResponseDto;
 import project.web.dto.PostsSaveRequestDto;
 import project.web.dto.PostsUpdateRequestDto;
 
-import javax.transaction.Transactional;
+//import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -22,15 +27,22 @@ public class PostsService {
 
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
-        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id ));
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
     }
 
-    public PostsResponseDto findById (Long id) {
+    public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
         return new PostsResponseDto(entity);
     }
-
+    // C U D 없는 서비스 메소드에서 사용 시 성능 개선.
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        // postsRepository 의 결과로 넘어온 Posts 의 스트림을 맵을 통해 PostsListResponseDto 로 변환하여 리스트로 반환하는 메소드.
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
 }
--- src/main/java/project/web/IndexController.java
+++ src/main/java/project/web/IndexController.java
@@ -1,13 +1,22 @@
 package project.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import project.service.PostsService;
 
+import org.springframework.ui.Model;
+@RequiredArgsConstructor // final 생성자 생성
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        // Model -> 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음.
+        // posts로 index.mustache로 전달
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
