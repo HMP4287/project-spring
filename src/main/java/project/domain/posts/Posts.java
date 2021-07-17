054e77f453808e7379442b1af64e23598f111861
HyunMinSam <hyunmin4287@naver.com>
2021-07-17 22:55:19 +0900

    BaseTimeEntity 클래스 완성 후 LocalDateTime 생성시간, 수정시간 테스트 코드 작성완료

--- src/main/java/project/domain/posts/Posts.java
+++ src/main/java/project/domain/posts/Posts.java
@@ -3,6 +3,7 @@ package project.domain.posts;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import project.domain.BaseTimeEntity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,7 +14,7 @@ import javax.persistence.Id;
 @Getter
 @NoArgsConstructor // 파라미터가 없는 기본 생성자를 생성해주고
 @Entity // JPA 어노테이션 , JPA에서는 엔티티는 테이블에 대응하는 하나의 클래스라고 생각
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/test/java/project/domain/posts/PostsRepositoryTest.java
+++ src/test/java/project/domain/posts/PostsRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,7 +26,7 @@ public class PostsRepositoryTest {
 
     @Test
     public void 게시글저장_불러오기() {
-        //given
+        // given
         String title = "테스트 게시글";
         String content = "테스트 본문";
 
@@ -35,12 +36,34 @@ public class PostsRepositoryTest {
                 .author("hyunmins4287@gmail.com")
                 .build());
 
-        //when
+        // when
         List<Posts> postsList = postsRepository.findAll();
 
-        //then
+        // then
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2021, 7, 16, 1, 11, 11);
+        postsRepository.save(Posts.builder()
+                .title("아 요즘 이랬다는데 ? ")
+                .content("그렇니 ?")
+                .author("익명")
+                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>> createDate = " + posts.getCreatedDate() + ", modifiedDate = " + posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+
     }
 }
