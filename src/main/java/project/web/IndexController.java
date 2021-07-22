021121238830f03c371e718a3891e75c1d652280
HyunMinSam <hyunmin4287@naver.com>
2021-07-22 23:58:12 +0900

    오류 해결

--- src/main/java/project/web/IndexController.java
+++ src/main/java/project/web/IndexController.java
@@ -17,21 +17,8 @@ import project.web.dto.PostsResponseDto;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
-
-    // UserLogin 어노테이션 추가 전
-//    @GetMapping("/")
-//    public String index(Model model) {
-//        // Model -> 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음.
-//        // posts로 index.mustache로 전달
-//        model.addAttribute("posts", postsService.findAllDesc());
-//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-//        if (user != null)
-//            model.addAttribute("userName", user.getName());
-//
-//        return "index";
-//    }
     // UserLogin 어노테이션 추가 후
+    @GetMapping("/")
     public String index(Model model, @LoginUser SessionUser user) {
         // Model -> 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음.
         // posts로 index.mustache로 전달
