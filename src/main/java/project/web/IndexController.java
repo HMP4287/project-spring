8cf9aaed081481fe2fecc05523ba440aefe57a4f
HyunMinSam <hyunmin4287@naver.com>
2021-07-22 22:41:03 +0900

    구글 로그인 생성, 세션 값 가져오는 부분을 어노테이션 기반으로 개선 (LoginUser 어노테이션)

--- src/main/java/project/web/IndexController.java
+++ src/main/java/project/web/IndexController.java
@@ -2,9 +2,11 @@ package project.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import project.config.auth.LoginUser;
+import project.config.auth.dto.SessionUser;
+import javax.servlet.http.HttpSession;
 import project.service.PostsService;
 
 import org.springframework.ui.Model;
@@ -15,12 +17,29 @@ import project.web.dto.PostsResponseDto;
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
-    @GetMapping("/")
-    public String index(Model model) {
+    // UserLogin 어노테이션 추가 전
+//    @GetMapping("/")
+//    public String index(Model model) {
+//        // Model -> 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음.
+//        // posts로 index.mustache로 전달
+//        model.addAttribute("posts", postsService.findAllDesc());
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        if (user != null)
+//            model.addAttribute("userName", user.getName());
+//
+//        return "index";
+//    }
+    // UserLogin 어노테이션 추가 후
+    public String index(Model model, @LoginUser SessionUser user) {
         // Model -> 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음.
         // posts로 index.mustache로 전달
         model.addAttribute("posts", postsService.findAllDesc());
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null)
+            model.addAttribute("userName", user.getName());
+
         return "index";
     }
 
